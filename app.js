83a24ab0aef739a49f74a176639dd922b9781c2f
xuxk13046567620 <839362863@qq.com>
2020-09-07 17:19:26 +0800

    xsadkalfsall12311

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ App({
     updateManager.onUpdateReady(function () {
       wx.showModal({
         title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
+        content: '新版本已准备好, 请重新进入.',
         success: function (res) {
           if (res.confirm) {
             // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
